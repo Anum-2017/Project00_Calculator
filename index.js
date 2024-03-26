92f65c81c988a7c5cc4631179d26d28530ae1b54
Anum-2017 <anumriz2017@gmail.com>
2024-03-26 14:26:33 +0500

    Calculator project

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "Enter 1st Number", type: "number", name: "firstNumber"
+    },
+    { message: "Enter 2nd Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select 1 of the operators",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+//conditional statement
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "/") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    console.log("Please select valid operator");
